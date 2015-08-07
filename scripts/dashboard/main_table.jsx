d3efcf0de09acf45370e10ee3c012158635e2679
zhuxiang <zhuxiang@qima-inc.com>
2015-08-07 11:42:10 +0800

    deal num

--- scripts/dashboard/main_table.jsx
+++ scripts/dashboard/main_table.jsx
@@ -5,11 +5,28 @@ let Table = mui.Table;
 module.exports = React.createClass({
   render: function () {
     let rowData = [
-      {linkName: {content: '注册'}, QPS: {content: '1000'}, trend: {content: '10%'}, RT: {content:'0.5'} },
+      {linkName: {content: '注册'}, QPS: {content:'1000'}, trend: {content: '10%'}, RT: {content:'0.5'} },
       {linkName: {content: '下单'}, QPS: {content: '1500'}, trend: {content: '5%'}, RT: {content:'0.3'} },
       {linkName: {content: '商品市场'}, QPS: {content: '1200'}, trend: {content: '5%'}, RT: {content:'0.4'} },
     ];
 
+
+    //转换数字
+    function wrapNum() {
+      rowData.forEach(function(item) {
+        for(var key in item){
+          var value = item[key].content;
+          if(React.Children.count(value) === 1){
+            if(!isNaN(value)){
+              value = <span>{value}</span>;
+            }
+          }
+          item[key].content = value;
+        }
+      })
+    };
+    wrapNum(rowData);
+    
     // State
     this.state = {
       fixedHeader: true,
