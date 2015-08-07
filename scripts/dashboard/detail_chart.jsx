f206e9f18af566f7c5bf489effe483d29189610b
zhuxiang <zhuxiang@qima-inc.com>
2015-08-07 21:33:03 +0800

    test

--- scripts/dashboard/detail_chart.jsx
+++ scripts/dashboard/detail_chart.jsx
@@ -1,24 +1,55 @@
 let React = require('react');
 let mui = require('material-ui');
+let Mixins = mui.Mixins;
+let StyleResizable = Mixins.StyleResizable;
 let SelectField = mui.SelectField;
 let TextField = mui.TextField;
 
-let DetailChart = React.createClass({
-  render: function () {
-    let menuItems = [
-      { payload: '1', text: 'Never' },
-      { payload: '2', text: 'Every Night' }
-    ];
+let menuItems = [{
+      payload: '1',
+      text: 'Never'
+    }, {
+      payload: '2',
+      text: 'Every Night'
+    }, {
+      payload: '3',
+      text: 'Weeknights'
+    }, {
+      payload: '4',
+      text: 'Weekends'
+    }, {
+      payload: '5',
+      text: 'Weekly'
+    }];
 
+let DetailChart = React.createClass({
+  displayName: 'TextFieldsPage',
+
+  mixins: [StyleResizable, React.addons.LinkedStateMixin],
+
+  getInitialState: function getInitialState() {
+    return {
+      selectValue: undefined
+    };
+  },
+  _handleSelectValueChange: function _handleSelectValueChange(name, e) {
+    var change = {};
+    change[name] = e.target.value;
+    this.setState(change);
+  },
+
+  render: function () {
+    
     return (
       <div>
         <div>
           监控页面选择：
-            <SelectField menuItems={menuItems} />
-            <TextField
-        hintText="Password Field"
-        floatingLabelText="Password"
-        type="password" />
+            <SelectField
+            style = {TextField}
+            value = {this.state.selectValue}
+            onChange = {this._handleSelectValueChange.bind(null, 'selectValue')}
+            hintText = '请选择'
+            menuItems = {menuItems}/>
         </div>
       </div>
     )
